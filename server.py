e36080913d470f95f086200ee8b79c6fd3bbf055
Charlotte Spencer <charlottelaspencer@gmail.com>
2015-01-12 15:26:30 +0000

    Update server.py comments

--- server.py
+++ server.py
@@ -7,8 +7,10 @@ HOST = "127.0.0.1"
 server_address = (HOST, PORT)
 
 # The CGIHTTPRequestHandler class allows us to run the cgi script in /cgi-bin/
-# Rather than attempt to show the file, which a 'BaseHTTPRequestHandler' or
+# Rather than attempt to display the cgi file itself, which a 'BaseHTTPRequestHandler' or
 # 'SimpleHTTPRequestHandler' may do
 httpd = http.server.HTTPServer(server_address, http.server.CGIHTTPRequestHandler)
 print("Starting my web server on port {0}".format(PORT))
+# Make sure the server is always serving the content
+# You can stop the server running using CTRL + C
 httpd.serve_forever()
